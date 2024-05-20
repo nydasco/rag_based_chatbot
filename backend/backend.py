d48092dec83c6817a2e8e7857deee8c1e8ccb278
Andy Sawyer <andy@nydas.co>
2024-05-20 21:07:04 +1000

    Give it a name.

--- backend/backend.py
+++ backend/backend.py
@@ -99,9 +99,9 @@ class RAGModel:
         )
 
         qa_system_prompt = """You are an assistant for question-answering tasks. \
-        Use the following pieces of retrieved context to answer the question. \
-        If you don't know the answer, just say that you don't know. Use \
-        three sentences maximum and keep the answer concise.\n\n{context}"""
+        You are named 'NydasBot'. Use the following pieces of retrieved context to \
+        answer the question. If you don't know the answer, just say that you don't know. \
+        Use three sentences maximum and keep the answer concise.\n\n{context}"""
 
         qa_prompt = ChatPromptTemplate.from_messages(
             [
